08eb2335aa2125c70773f1bba2970b674c586ed1
yuichiro12 <talcnrhyme@gmail.com>
2018-04-10 14:43:04 +0900

    epsilon-greedy

--- bandit.go
+++ bandit.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -11,14 +12,32 @@ func regret (selection []int) int {
 func epsilonGreedy (arms []int, epsilon float64, selectCnt int) {
 	// initialize
 	armsCnt := len(arms)
-	exploreCnt := int(math.Ceil(epsilon * float64(selectCnt)))
-
-	// exploration
-	for i := 0; i < exploreCnt; i++ {
-		slot(arms[i % armsCnt])
+	armsTest := make([][]int, armsCnt)
+	for i := range armsTest {
+		armsTest[i] = make([]int, 0)
 	}
 
+	// exploration
+	exploreCnt := int(math.Ceil(epsilon * float64(selectCnt)))
+	for i := 0; i < exploreCnt; i++ {
+		currentArm := i % armsCnt
+		armsTest[currentArm] = append(armsTest[currentArm], slot(arms[currentArm]))
+	}
+	armMeans := make([]float64, armsCnt)
+	for i, testi := range armsTest {
+		armMeans[i] = mean(testi)
+	}
+	fmt.Println(armMeans)
+	bestArm := arms[findMaxIndex(armMeans)]
+
 	// exploitation
+	exploitCnt := selectCnt - exploreCnt
+	armsExploit := make([]int, exploitCnt)
+	for i := 0; i < exploitCnt; i++ {
+		armsExploit[i] = slot(bestArm)
+	}
+	fmt.Println(armsTest)
+	fmt.Println(armsExploit)
 }
 
 func sum (slice []int) int {
@@ -31,4 +50,15 @@ func sum (slice []int) int {
 
 func mean (slice []int) float64 {
 	return float64(sum(slice)) / float64(len(slice))
+}
+
+func findMaxIndex (slice []float64) int {
+	maxv := 0.0
+	maxi := 0
+	for i, v := range slice {
+		if v > maxv {
+			 maxi = i
+		}
+	}
+	return maxi
 }
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main () {
 	arms := []int{10, 20, 30, 40, 50}
-	for i, v := range arms {
-		fmt.Println("slot", i+1, ":", slot(v))
-	}
+	epsilon := 0.3
+	selectCnt := 101
+	epsilonGreedy(arms, epsilon, selectCnt)
 }
